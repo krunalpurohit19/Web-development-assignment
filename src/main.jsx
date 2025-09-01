d73d7117c1172dc1d69dd99cdee4bdb5add6aa1f
kpurohit009 <kpurohit009@gmail.com>
2025-09-01 19:11:59 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { inject } from "@vercel/analytics"
+
+inject()
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  // </StrictMode>,
 )
